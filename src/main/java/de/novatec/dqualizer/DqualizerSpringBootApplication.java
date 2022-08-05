6d7e6187a43265f3dc866fe5f7124caf233c4b6c
MatthiasEschhold <84323232+MatthiasEschhold@users.noreply.github.com>
2022-08-05 11:41:34 +0200

    inital setup

---
+++ src/main/java/de/novatec/dqualizer/DqualizerSpringBootApplication.java
@@ -0,0 +1,10 @@
+package de.novatec.dqualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class DqualizerSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DqualizerSpringBootApplication.class, args);
+    }
+}
