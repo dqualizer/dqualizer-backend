bc7837c4667417f97eb5bd88d2af516d16494076
Heiko Holz <heiko.holz@novatec-gmbh.de>
2022-08-11 13:37:25 +0200

    add Kotlin support; disable dependencies resulting in exceptions

---
+++ src/main/kotlin/de/novatec/dqualizer/DqualizerKotlinSpringBootApplication.kt
@@ -0,0 +1,12 @@
+package de.novatec.dqualizer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DqualizerKotlinSpringBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<DqualizerKotlinSpringBootApplication>(*args)
+}
+
